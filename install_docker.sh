4200432b5ba71bd858596cdec70fd656d7a5187a
flhuillery <46817466+flhuillery@users.noreply.github.com>
2025-03-14 09:21:20 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Add Docker's official GPG key:
-apt-get update
-apt-get -y dist-upgrade
-apt-get -y install ca-certificates curl
+apt update
+apt -y dist-upgrade
+apt -y install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,8 +15,8 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker engine & compose
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose gpg
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose gpg
 
 # Run Docker Hello World
  docker run --name hello-world -d hello-world
@@ -30,12 +30,6 @@ if [ "$reponse" == "o" ]; then
 else
     echo "Opération annulée. Docker n'a pas été supprimé."
 fi
-
-# Add user docker
-/sbin adduser dockeruser
-echo "Entrer le mot de passe pour l'utilisateur dockeruser"
-passwd dockeruser
-/sbin/sbinusermod -aG docker dockeruser
 
 # Rebbot
 read -p "Voulez-vous reboot ? (o/n) : " reponse
